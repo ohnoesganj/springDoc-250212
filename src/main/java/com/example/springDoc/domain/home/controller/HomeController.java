935d2c974d3757c1b840911631d1590d3734cd1f
ohnoesganj <sunho3456@naver.com>
2025-02-12 11:26:39 +0900

    swagger-ui 접속 설정

---
+++ src/main/java/com/example/springDoc/domain/home/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.springDoc.domain.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "API 서버에 오신 걸 환영합니다.";
+    }
+}
