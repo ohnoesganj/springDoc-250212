f4e5df9e3fb339418c458173637f6c18f0194c47
ohnoesganj <sunho3456@naver.com>
2025-02-12 10:37:14 +0900

    First Commit

---
+++ src/test/java/com/example/springDoc/SpringDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
